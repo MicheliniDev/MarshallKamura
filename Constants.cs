4097d0228d787c8db89364c4a453b02716cafde5
MicheliniDev <michelinidev@gmail.com>
2026-01-18 20:38:21 -0300

    Fixed typos and buffed his HP from 5500 to 6000

--- Constants.cs
+++ Constants.cs
@@ -4,20 +4,20 @@ namespace KamuraPrime
 {
     public static class Constants
     {
-        public const string BOSS_NAME = "Marshall Kamura";
+        public const string BOSS_NAME = "Marshal Kamura";
 
         public const string BOSS_KEY = "KamuraPrime";
-        public const string BOSS_CHALLENGE_TEXT = "Marshall Challenge";
+        public const string BOSS_CHALLENGE_TEXT = "Marshal Challenge";
 
         public const string P2_ENTER_STATE = "Triple Spread Combo";
         public const string DEBUG_NODE_NAME = "Triple Shotgun Combo";
 
-        public const float MAX_HP = 5500f;
+        public const float MAX_HP = 6000f;
         public static readonly float[] PHASE_THRESHOLDS = [.51f, 0f];
         public static readonly float[] WEAK_POINT_RANGES = [1f, .85f, .7f];
 
         public static readonly float[] ORIGINAL_DAMAGES = [0f, 125f, 250f, 500f, 0f];
-        public static readonly float[] KAMURA_DAMAGES = [0f, 65f, 115f, 215f, 0f];
+        public static readonly float[] KAMURA_DAMAGES = [0f, 45f, 95f, 195f, 0f];
 
         public static readonly Vector2 ARENA_TOP_POS = new Vector2(2420f, -268f);
 
